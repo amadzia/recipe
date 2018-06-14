72911f61cca8140550a1e7a8c6669b1f023b884c
amadzia <agnieszkamadzia@onet.pl>
2018-06-14 12:19:07 +0200

    Display list of recipes

--- src/main/java/com/example/recipe/domain/Ingredient.java
+++ src/main/java/com/example/recipe/domain/Ingredient.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 public class Ingredient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
@@ -17,6 +18,16 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/example/recipe/services/RecipeService.java
@@ -0,0 +1,11 @@
+package com.example.recipe.services;
+
+import com.example.recipe.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
