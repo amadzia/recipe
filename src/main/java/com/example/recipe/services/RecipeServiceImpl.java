c0b338185f67f44947c7b3a97dd73be9c1655c9d
amadzia <agnieszkamadzia@onet.pl>
2018-06-14 15:50:17 +0200

    Project Lombok added

--- src/main/java/com/example/recipe/services/RecipeServiceImpl.java
+++ src/main/java/com/example/recipe/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.recipe.services;
 
 import com.example.recipe.domain.Recipe;
 import com.example.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+
+        log.debug("log from service");
 
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
